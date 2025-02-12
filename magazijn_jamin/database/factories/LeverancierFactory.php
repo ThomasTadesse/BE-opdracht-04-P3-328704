ac46236b4e2a210f0c888f88729452eca4439bfa
ThomasTadesse <328704@student.mboutrecht.nl>
2025-02-12 10:47:55 +0100

    leverancier factory. INSERT DATA

--- magazijn_jamin/database/factories/LeverancierFactory.php
+++ magazijn_jamin/database/factories/LeverancierFactory.php
@@ -13,6 +13,85 @@ use Carbon\Carbon;
 class LeverancierFactory extends Factory
 {
     protected $model = Leverancier::class;
+
+    private static $leveranciers = [
+        [
+            'ContactId' => 1,
+            'Naam' => 'Venco',
+            'Contactpersoon' => 'Bert van Linge',
+            'Leveranciernummer' => 'L1029384719',
+            'Mobiel' => '06-28493827',
+            'IsActief' => 1,
+            'Opmerkingen' => null,
+            'DatumAangemaakt' => '2024-11-22 00:00:00',
+            'DatumGewijzigd' => '2024-11-22 00:00:00',
+        ],
+        [
+            'ContactId' => 2,
+            'Naam' => 'Astra Sweets',
+            'Contactpersoon' => 'Jasper del Monte',
+            'Leveranciernummer' => 'L1029284315',
+            'Mobiel' => '06-39398734',
+            'IsActief' => 1,
+            'Opmerkingen' => null,
+            'DatumAangemaakt' => '2024-11-22 00:00:00',
+            'DatumGewijzigd' => '2024-11-22 00:00:00',
+        ],
+        [
+            'ContactId' => 3,
+            'Naam' => 'Haribo',
+            'Contactpersoon' => 'Sven Stalman',
+            'Leveranciernummer' => 'L1029324748',
+            'Mobiel' => '06-24383291',
+            'IsActief' => 1,
+            'Opmerkingen' => null,
+            'DatumAangemaakt' => '2024-11-22 00:00:00',
+            'DatumGewijzigd' => '2024-11-22 00:00:00',
+        ],
+        [
+            'ContactId' => 4,
+            'Naam' => 'Basset',
+            'Contactpersoon' => 'Joyce Stelterberg',
+            'Leveranciernummer' => 'L1023845773',
+            'Mobiel' => '06-48293823',
+            'IsActief' => 1,
+            'Opmerkingen' => null,
+            'DatumAangemaakt' => '2024-11-22 00:00:00',
+            'DatumGewijzigd' => '2024-11-22 00:00:00',
+        ],
+        [
+            'ContactId' => 5,
+            'Naam' => 'De Bron',
+            'Contactpersoon' => 'Remco Veenstra',
+            'Leveranciernummer' => 'L1023857736',
+            'Mobiel' => '06-34291234',
+            'IsActief' => 1,
+            'Opmerkingen' => null,
+            'DatumAangemaakt' => '2024-11-22 00:00:00',
+            'DatumGewijzigd' => '2024-11-22 00:00:00',
+        ],
+        [
+            'ContactId' => 6,
+            'Naam' => 'Quality Street',
+            'Contactpersoon' => 'Johan Nooij',
+            'Leveranciernummer' => 'L1029234586',
+            'Mobiel' => '06-23458456',
+            'IsActief' => 1,
+            'Opmerkingen' => null,
+            'DatumAangemaakt' => '2024-11-22 00:00:00',
+            'DatumGewijzigd' => '2024-11-22 00:00:00',
+        ],
+        [
+            'ContactId' => 7,
+            'Naam' => 'Hom Ken Food',
+            'Contactpersoon' => 'Hom Ken',
+            'Leveranciernummer' => 'L1029234599',
+            'Mobiel' => '06-23458477',
+            'IsActief' => 1,
+            'Opmerkingen' => null,
+            'DatumAangemaakt' => '2024-11-22 00:00:00',
+            'DatumGewijzigd' => '2024-11-22 00:00:00',
+        ]];
     /**
      * Define the model's default state.
      *
@@ -20,13 +99,6 @@ class LeverancierFactory extends Factory
      */
     public function definition(): array
     {
-        return [
-            'Naam' => $this->faker->name,
-            'Contactpersoon' => $this->faker->name,
-            'Leveranciernummer' => Str::random(10),
-            'Mobiel' => Str::random(10),
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-        ];
+        return self::$leveranciers[array_rand(self::$leveranciers)];
     }
 }

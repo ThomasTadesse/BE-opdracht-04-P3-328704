1e9aca389ee3eb121cc466be3926fba6cad3accf
ThomasTadesse <328704@student.mboutrecht.nl>
2025-02-12 11:03:15 +0100

    product factory

--- magazijn_jamin/database/seeders/DatabaseSeeder.php
+++ magazijn_jamin/database/seeders/DatabaseSeeder.php
@@ -30,8 +30,8 @@ class DatabaseSeeder extends Seeder
 
         Leverancier::factory()->count(7)->create();
         Contact::factory()->count(6)->create();
+        Product::factory()->count(14)->create();
         
-        // ::factory()->count( )->create();
         // ::factory()->count( )->create();
         // ::factory()->count( )->create();
         // ::factory()->count( )->create();

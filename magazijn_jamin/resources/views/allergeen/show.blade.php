168cf9162135f6a95257fc8a0257a3de776900f0
ThomasTadesse <328704@student.mboutrecht.nl>
2025-02-18 13:15:00 +0100

    toggle voor unhappy scenario contactgegevens

--- magazijn_jamin/resources/views/allergeen/show.blade.php
+++ magazijn_jamin/resources/views/allergeen/show.blade.php
@@ -23,35 +23,56 @@
                 </tr>
             </thead>
             <tbody>
-                @if($allergeen)
                 <tr class="border-b hover:bg-gray-50">
+                    <!-- leverancier gegevens -->
                     <td class="py-3 px-4">{{ $allergeen->LeverancierNaam ?? 'N/A' }}</td>
                     <td class="py-3 px-4">{{ $allergeen->Contactpersoon ?? 'N/A' }}</td>
                     <td class="py-3 px-4">{{ $allergeen->Mobiel ?? 'N/A' }}</td>
-                    
-                    <!-- Adresgegevens contact -->
-                    @if(isset($allergeen->Stad) && isset($allergeen->Straat) && isset($allergeen->Huisnummer))
-                        <td class="py-3 px-4">{{ $allergeen->Stad }}</td>
-                        <td class="py-3 px-4">{{ $allergeen->Straat }}</td>
-                        <td class="py-3 px-4">{{ $allergeen->Huisnummer }}</td>
-                    @else
-                        <td class="py-3 px-4" colspan="3">Er zijn geen adresgegevens bekend</td>
-                    @endif
+
+                    <!-- Address data container -->
+                    <td colspan="3" class="py-3 px-4">
+                        <!-- No address message (visible by default) -->
+                        <span class="no-address-message bg-gray-200 rounded">Er zijn geen adresgegevens bekend</span>
+                        
+                        <!-- Address data (hidden by default) -->
+                        @if(isset($allergeen->Stad) && isset($allergeen->Straat) && isset($allergeen->Huisnummer))
+                        <span class="address-data" style="display: none;">
+                            <span class="mr-4">{{ $allergeen->Stad }}</span>
+                            <span class="mr-4">{{ $allergeen->Straat }}</span>
+                            <span>{{ $allergeen->Huisnummer }}</span>
+                        </span>
+                        @endif
+                    </td>
                 </tr>
-                @else
-                <tr>
-                    <td colspan="6" class="py-3 px-4 text-center">Geen gegevens gevonden</td>
-                </tr>
-                @endif
             </tbody>
         </table>
 
-        <!-- Terugknop -->
-        <div class="mt-6">
+        <!-- Knoppen -->
+        <div class="mt-6 space-x-4">
             <a href="{{ url()->previous() }}" class="bg-blue-300 text-white px-4 py-2 rounded-lg shadow hover:bg-green-300">
                 Terug naar vorige pagina
             </a>
+            <button onclick="toggleAddress()" class="bg-gray-300 text-white px-4 py-2 rounded-lg shadow hover:bg-red-300">
+                Toon Contact Data
+            </button>
         </div>
     </div>
+
+    <script>
+        function toggleAddress() {
+            const addressData = document.querySelector('.address-data');
+            const noAddressMessage = document.querySelector('.no-address-message');
+            
+            if (addressData.style.display === 'none') {
+                addressData.style.display = 'inline';
+                noAddressMessage.style.display = 'none';
+            } else {
+                addressData.style.display = 'none';
+                noAddressMessage.style.display = 'inline';
+            }
+        }
+
+        // No need for DOMContentLoaded event as the initial state is handled by HTML
+    </script>
 </body>
 </html>
